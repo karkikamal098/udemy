d560e371a9ec8f2526a44eae403f6c783b845960
karkikamal098 <kamaljungkarki13579@gmail.com>
2024-10-15 18:32:45 +0545

    rest api

--- API/restapi/solution.js
+++ API/restapi/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "fa7ef06b-bc6a-4d41-aaa7-f313362e2ab2";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
