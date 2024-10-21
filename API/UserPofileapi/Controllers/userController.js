c404357ba9ece995cfce437c32539fbd28e979d7
karkikamal098 <kamaljungkarki13579@gmail.com>
2024-10-21 21:01:50 +0545

    own profile

--- API/UserPofileapi/Controllers/userController.js
+++ API/UserPofileapi/Controllers/userController.js
@@ -1,29 +1,56 @@
-import User from '../Models/userModels.js';
+import User from "../Models/userModels.js";
+import bcrypt from "bcrypt";
 
-
-export const registerUser = (req, res) => {
+export const registerUser = async (req, res) => {
   try {
-    res.json("aa");
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists." });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully." });
   } catch (e) {
     res.status(500).json(e.message);
   }
 };
 
-
-export const loginUser = (req,res) => {
+export const loginUser = async(req, res) => {
   try {
-    res.json("bb");
+    const { Email, Password } = req.body;
+    console.log(Email, Password);
+    const emailSaved = await User.findOne({ email: newEmail });
+    if (!emailSaved) {
+      return res.status(401).json({ message: "User not found." });
+    } else {
+      const eMatch = await bcrypt.compare(Password, emailSaved.password);
+      console.log(eMatch);
+
+      if (!eMatch) {
+        return res.status(401).json({ message: "Invalid password." });
+      }
+
+      res
+        .status(200)
+        .json({ message: "user details is correct and is logged in." });
+    }
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
-export const deleteUser = (req,res) => {
+export const deleteUser = (req, res) => {
   try {
     res.json("cc");
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
-
-
--- API/UserPofileapi/Models/userModels.js
+++ API/UserPofileapi/Models/userModels.js
@@ -3,12 +3,19 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
   name: {
     type: String,
+    required: true,
+    minlength: 5,
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
+    required: true,
+    minlength: 8,
+    select: false, // hide password in response
   },
 });
 
